33fb959a52c31e5346067c3bc001cd0842fb4a60
GACiobanita <ciobanita_alex@yahoo.com>
2019-08-09 16:13:10 +0100

    Modified functionality of DatabaseArchitecture

--- database_functionality/database_architect.py
+++ database_functionality/database_architect.py
@@ -9,30 +9,28 @@ class DatabaseArchitect(object):
     def __init__(self):
         self.db_connection = None
         self.db_cursor = None
-        self.current_table = None
-        self.current_database = None
+        self.db_tables = []
+        self.csv_files = []
 
-    def create_db_connection(self, db_file):
+        module_file_path = os.path.abspath(getsourcefile(lambda: 0))
+
+        project_file_path = os.path.dirname(os.path.dirname(module_file_path))
+
+        self.data_base_file_path = project_file_path + "\\database\\vendor_database.db"
+        self.csv_file_path = project_file_path + "\\csv_files"
+
+        self.connect_to_database()
+
+    def create_db_connection(self):
         # create a database_functionality connection to a SQLite database_functionality
         try:
-            self.db_connection = sqlite3.connect(db_file)
+            self.db_connection = sqlite3.connect(self.data_base_file_path)
             print(sqlite3.version)
         except Exception as e:
             print(e)
 
     def create_db_cursor(self):
         self.db_cursor = self.db_connection.cursor()
-
-    def create_table(self, table_name):
-        execution_string = '''CREATE TABLE {} (Item_ID text NOT NULL)'''.format(table_name)
-        self.db_cursor.execute(execution_string)
-
-    def drop_table(self, table_name):
-        execution_string = '''DROP TABLE {}'''.format(table_name)
-        self.db_cursor.execute(execution_string)
-
-    def add_table_column_with_basic_type(self, table_name, column_name, column_type):
-        self.db_cursor.execute('''ALTER TABLE {} ADD COLUMN {} {}'''.format(table_name, column_name, column_type))
 
     def add_data_to_table(self, table_name):
         table_columns = self.get_table_columns(table_name)
@@ -44,10 +42,17 @@ class DatabaseArchitect(object):
         execution_string = 'INSERT INTO {} VALUES ('.format(table_name) + column_count[:-1] + ')'
         self.db_cursor.executemany(execution_string, (input_values,))
 
-    def get_table_from_db(self):
+    def distribute_data_to_table(self, new_table_values, table_name):
+        table_columns = self.get_table_columns(table_name)
+        input_values = new_table_values
+        column_count = '?,' * len(table_columns)
+        execution_string = 'INSERT INTO {} VALUES ('.format(table_name) + column_count[:-1] + ')'
+        self.db_cursor.executemany(execution_string, (input_values,))
+
+    def get_tables_from_database(self):
         result = self.db_cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
         for table_name in result:
-            print(table_name[0])
+            self.db_tables.append(table_name[0])
 
     def get_table_columns(self, table_name):
         execution_string = '''PRAGMA table_info({})'''.format(table_name)
@@ -55,7 +60,7 @@ class DatabaseArchitect(object):
         rows = self.db_cursor.fetchall()
         return rows
 
-    def commit_changes_to_db(self):
+    def commit_changes_to_database(self):
         self.db_connection.commit()
 
     def commit_and_close_db(self):
@@ -65,12 +70,47 @@ class DatabaseArchitect(object):
     def close_db(self):
         self.db_connection.close()
 
-    def csv_to_db(self, data_base_name, csv_table_name, item_list='item_list.csv'):
-        module_file_path = os.path.abspath(getsourcefile(lambda: 0))
-        csv_file_path = os.path.join(os.path.dirname(module_file_path), item_list)
-        db_file_path = os.path.dirname(os.path.dirname(module_file_path)) + "\\database\\" + data_base_name
-        print(db_file_path)
+    def create_new_database(self):
         self.create_db_connection()
 
-        df = pd.read_csv(csv_file_path)
-        df.to_sql("test_csv_table", self.db_connection, if_exists='append', index=False)
+    def get_csv_files_from_path(self):
+        file_names = os.listdir(self.csv_file_path)
+        for file_name in file_names:
+            if file_name.endswith(".csv"):
+                self.csv_files.append(file_name)
+
+    def csv_to_database_table(self):
+        # in order to avoid rogue creation of databases
+        # we want to control how databases are created through our code
+        for csv_file_name in self.csv_files:
+            df = pd.read_csv(self.csv_file_path+"\\"+csv_file_name)
+            df.to_sql(csv_file_name[:-4], self.db_connection, if_exists='append', index=False)
+
+    def get_data_base_data(self):
+        df_list = []
+        for table in self.db_tables:
+            df = pd.read_sql(sql='SELECT * FROM {}'.format(table), con=self.db_connection, index_col=None)
+            df_list.append((table, df))
+        return df_list
+
+
+    def data_base_to_csv(self, csv_file_path):
+        for table in self.db_tables:
+            df = pd.read_sql(sql='SELECT * FROM {}'.format(table), con=self.db_connection)
+            df.to_csv(csv_file_path + str(table) + ".csv", index=False)
+
+    def connect_to_database(self):
+        if os.path.isfile(self.data_base_file_path) is True:
+            self.create_db_connection()
+            self.create_db_cursor()
+            self.get_tables_from_database()
+            print("Successfully connect to existent data base.")
+        else:
+            print("Data base does not exist, creating new data base.\n")
+            self.create_new_database()
+            print("Data base created, uploading data from csv file.\n")
+            self.get_csv_files_from_path()
+            self.csv_to_database_table()
+            print("New database created.\n")
+            self.create_db_cursor()
+            self.get_tables_from_database()
--- database_functionality/database_functionality_tests.py
+++ database_functionality/database_functionality_tests.py
@@ -5,39 +5,11 @@ import unittest
 class TestDatabaseFunctionality(unittest.TestCase):
 
     def setUp(self):
-        self.database_architect = DatabaseArchitect(
-            'D:\\Python\\vending-machine\\vending-machine\\database\\test_db.db')
+        self.database_architect = DatabaseArchitect()
 
-    def test_create_table_valid(self):
-        self.database_architect.create_table("test_table_name")
-        self.database_architect.commit_and_close_db()
-        self.assertNotEqual(None, self.database_architect.db_connection)
+    def test_get_data_base_table_valid(self):
+        self.database_architect.get_data_base_data()
 
-    def test_get_table_name_valid(self):
-        self.database_architect.get_table_from_db()
-        self.database_architect.close_db()
-        self.assertNotEqual(None, self.database_architect.db_connection)
-
-    def test_delete_table_valid(self):
-        self.database_architect.drop_table("test_table_name")
-        self.database_architect.commit_and_close_db()
-        self.assertNotEqual(None, self.database_architect.db_connection)
-
-    def test_create_table_column_with_basic_type(self):
-        self.database_architect.add_table_column_with_basic_type("test_table_name", "Item_Price", "text")
-        self.database_architect.commit_and_close_db()
-
-    def test_get_table_columns(self):
-        self.database_architect.get_table_columns("test_table_name")
-        self.database_architect.close_db()
-
-    def test_add_data_to_table(self):
-        self.database_architect.add_data_to_table("test_table_name")
-        self.database_architect.close_db()
-
-    def test_csv_to_db(self):
-        self.database_architect.csv_to_db('test_csv_to_db.db', "csv_table")
-        self.database_architect.close_db()
 
 if __name__ == "__main__":
     unittest.main()
